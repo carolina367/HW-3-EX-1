bb032c080971d80a3d02a2133c462e09c5070cec
carolina367 <43555286+carolina367@users.noreply.github.com>
2024-10-03 18:49:57 -0500

    Creating Table

--- answers.sh
+++ answers.sh
@@ -1,18 +1,19 @@
 # problem 0:
-SELECT * FROM Students;
+SELECT * FROM students;
 
 # Basic queries
-SELECT Name FROM Students;
-SELECT * FROM Students WHERE Age > 30;
-SELECT Name FROM Students WHERE Gender='F' AND Age=30;
-SELECT Points FROM Students WHERE Name='Alex';
-INSERT INTO Students VALUES(147, 'Carolina', '22', 'F', 500);
-UPDATE Students SET Points = 500 WHERE Name='Basma';
-UPDATE Students SET Points = 100 WHERE Name='Alex';
+SELECT Name FROM students;
+SELECT * FROM students WHERE Age > 30;
+SELECT Name FROM students WHERE Gender='F' AND Age=30;
+SELECT Points FROM students WHERE Name='Alex';
+INSERT INTO students VALUES(147, 'Carolina', '22', 'F', 500);
+UPDATE students SET Points = 500 WHERE Name='Basma';
+UPDATE students SET Points = 100 WHERE Name='Alex';
 
+# Create Table
 CREATE TABLE graduates(
-    ID INTEGER  PRIMARY KEY AUTOINCREMENT,
-    Name  TEXT NOT NULL UNIQUE,
+    ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    Name TEXT NOT NULL UNIQUE,
     Age INTEGER,
 	Gender TEXT,
 	Points INTEGER,
